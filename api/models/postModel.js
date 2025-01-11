cb5cfa9293e91c455c2e1fc389a64b0aba27b915
Varin Singh <varunpratap74358@gmail.com>
2025-01-12 01:15:17 +0900

    Models are complited

---
+++ api/models/postModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+})
+
+export const Post = mongoose.model('Post', postSchema)
