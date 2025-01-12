9b596c92d5d5cd9c99a1f4c9afc1d62ef8d6a8b1
Varin Singh <varunpratap74358@gmail.com>
2025-01-12 21:02:24 +0900

    cloudinary, multer and server.js is commplited

--- api/server.js
+++ api/server.js
@@ -1,1 +1,26 @@
-asdfg
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("DB is connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
